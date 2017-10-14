7c8c9ca98e32eb3e7775828588884da1eca9bf4d
patrick <p.barattin@gmail.com>
2017-10-15 00:46:48 +0200

    more stuf

--- database.js
+++ database.js
@@ -12,37 +12,62 @@ var connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
-  });
+});
 
 var db = {};
-db.userAlreadyExist = function (chat_id) {
-     connection.query("SELECT count * FROM users WHERE telegram_ID  = ?", [chat_id],)
-      console.log( "aaa: ", result)
-    // TODO vedere com'è il result e ritornare di conseguenza
-    // BOOLEAN
-};
+/*db.userAlreadyExist = function (chat_id) {
 
-db.getUserIdFromChatId = function (chat_id) {
-    return connection.query("SELECT users.id FROM users WHERE users.telegram_ID = ?", chat_id)
-}
+    return new Promise(function (resolve, reject) {
+        connection.query("SELECT * FROM users WHERE telegram_ID  = ?", [chat_id], function (error, result, fields) {
+            if (error) reject(error);
+            resolve(result.lenght > 0);
+        });
+    });
+};*/
 
-db.setUser = function (chat_id) {
-    if (db.userAlreadyExist(chat_id) == false) {
-        connection.query("INSERT INTO users(telegram_ID) values(?)", chat_id)
-    }
-}
+/*db.getUserIdFromChatId = function (chat_id) {
+    return new Promise(function (resolve, reject) {
+        connection.query("SELECT id FROM users WHERE telegram_ID = ?", chat_id, function (error, result, fields) {
+            if (error) reject(error);
+            resolve(result[0]);
+        });
+    })
+
+}*/
+
+/*db.setUser = function (chat_id) {
+    connection.query("INSERT INTO users(telegram_ID) values(?)", chat_id)
+
+}*/
 
 db.getNearbyAirports = function (lat, long) {
-    return connection.query()
+    return new Promise(function (resolve, reject) {
+        connection.query("SELECT name, iata_code, ( 3959 * acos( cos( radians(?) ) * cos( radians( latitude ) ) " +
+            "* cos( radians( longitude ) - radians(?) ) + sin( radians(?) ) * sin(radians(latitude)) ) ) AS distance " +
+            "FROM airports " +
+            "ORDER BY DISTANCE " +
+            "LIMIT 10", [lat, long, lat],
+            function (error, results, fields) {
+                if (error) reject(error);
+                resolve(results);
+            })
+    });
 }
-
 db.connectUserToAirport = function (chat_id, airport_id) {
 
-    connection.query("INSERT INTO users_airports(user_id, airport_id) values(?,?)", [getUserIdFromChatId, airport_id])
+    connection.query("INSERT INTO users_airports(chat_id, airport_id) values(?,?)", [chat_id, airport_id])
 }
 
-db.hasUserSetLocation = function (chat_id) {
-    connection.query("SELECT user.id FORM users")
+db.userLocations = function (user_id) {
+    return new Promise(function (resolve, reject) {
+        connection.query("SELECT iata_code FROM users_airports WHERE chat_id = ?", chat_id, function (error, result, fields) {
+            if (error) reject(error);
+            console.log("userlocation: ", JSON.stringify(result))
+            resolve(result);
+        })
+    })
+
+
 
     // BOOLEAN
 }
--- skyscannerbot.js
+++ skyscannerbot.js
@@ -11,10 +11,11 @@ const {
 const bot = new Telegraf('464972549:AAEUgiGOLnwJ277BWHtmkCd0up3rLCey5bI')
 const Database = require('./database')
 
-const aereoports = []
+const airports = []
 const user_language_code = 'en'
 
 intro_message = "Welcome to our bot, we will notify you all the flight under 30€ \n But first send to your position to select some departure airports"
+airports_message = "Select the airports that you will be notice"
 
 
 /*app.get('/', function (req, res) {
@@ -26,7 +27,7 @@ app.listen(3000, function () {
 })
 */
 
-//bot.use(Telegraf.log())
+bot.use(Telegraf.log())
 
 function sendFlights(chat_id, data) {
   console.log(chat_id)
@@ -38,9 +39,6 @@ function sendFlights(chat_id, data) {
   bot.telegram.sendMessage(chat_id, data.message, linkToFlight)
 }
 
-function log(msg){
-  console.log(msg)
-}
 
 function getLocation(ctx, message, text) {
   return ctx.reply(message, Extra.markup((markup) => {
@@ -50,6 +48,22 @@ function getLocation(ctx, message, text) {
       ])
   }))
 }
+/*
+function selectAirport(ctx, airports, message){
+  return ctx.reply(message, Extra.markup((markup)=>{
+    return markup.resize()
+    .keyboard(airports.map(function(a){return "✈️ " + a.name}))
+  }))
+}*/
+
+
+function selectAirport(ctx, airports, message) {
+  return ctx.reply(message, Markup.inlineKeyboard([
+    airports.map(function (a) {
+      return  Markup.callbackButton("✈️ " + a.name, a.iata_code)
+    })
+  ]))
+}
 
 function getChatId(ctx) {
   return ctx.update.message.chat.id
@@ -57,12 +71,20 @@ function getChatId(ctx) {
 
 
 bot.on('location', (ctx) => {
-  console.log(ctx.update.message.location)
-  data = {
-    "message": "Barcellona - Parigi 30€",
-    "link": "https://www.skyscanner.net/transport_deeplink/4.0/UK/en-gb/GBP/vuel/1/9772.12404.2017-10-19/air/airli/flights?itinerary=flight%7C-31685%7C3540%7C9772%7C2017-10-19T07%3A00%7C12404%7C2017-10-19T08%3A00&carriers=-31685&operators=-31685&passengers=1&channel=website&cabin_class=economy&facilitated=false&ticket_price=22.52&is_npt=false&is_multipart=true&client_id=skyscanner_website&request_id=805e22f3-b878-4c80-b9fc-dcaf32e1a21b&commercial_filters=false&q_datetime_utc=2017-10-14T11%3A20%3A08&isbp=1&tabs=CombinedDayView&qid=9772-1710190700--31685-0-12404-1710190800%7C12404-1710260825--31915-0-9772-1710260930&sort=fqsscore&index=0&posidx=0&stops=0%2C0&pre_redirect_id=c3fc054f-1031-4bf0-8efb-daae120458c6"
-  };
-  sendFlights(ctx.message.chat.id, data)
+  location = ctx.update.message.location
+  console.log(location)
+  Database.getNearbyAirports(location.latitude, location.longitude).then(function (airports) {
+    console.log(JSON.stringify(airports))
+    selectAirport(ctx, airports, airports_message)
+
+  }).catch(function (error) {
+    console.log(error)
+  })
+  /* data = {
+     "message": "Barcellona - Parigi 30€",
+     "link": "https://www.skyscanner.net/transport_deeplink/4.0/UK/en-gb/GBP/vuel/1/9772.12404.2017-10-19/air/airli/flights?itinerary=flight%7C-31685%7C3540%7C9772%7C2017-10-19T07%3A00%7C12404%7C2017-10-19T08%3A00&carriers=-31685&operators=-31685&passengers=1&channel=website&cabin_class=economy&facilitated=false&ticket_price=22.52&is_npt=false&is_multipart=true&client_id=skyscanner_website&request_id=805e22f3-b878-4c80-b9fc-dcaf32e1a21b&commercial_filters=false&q_datetime_utc=2017-10-14T11%3A20%3A08&isbp=1&tabs=CombinedDayView&qid=9772-1710190700--31685-0-12404-1710190800%7C12404-1710260825--31915-0-9772-1710260930&sort=fqsscore&index=0&posidx=0&stops=0%2C0&pre_redirect_id=c3fc054f-1031-4bf0-8efb-daae120458c6"
+   };*/
+  //sendFlights(ctx.message.chat.id, data)
 })
 
 
@@ -73,22 +95,19 @@ bot.command('respond_with_aereopots', (ctx) => {
 })
 
 bot.command('/start', (ctx) => {
-  
+
   chat_id = getChatId(ctx)
-  log("chat id: " + chat_id)
-  if (Database.userAlreadyExist(chat_id)) {
-    // sarebbe figo stampargli anche la sua posizione attuale (ovvero g)
-    if (Database.hasUserSetLocation(chat_id)) getLocation(ctx,"Do you want to change your position?", "Change you position")
-    else getLocation(ctx,intro_message, "Set location")
-  } else {
-    Database.setUser(chat_id)
-    getLocation(ctx,intro_message, "Set location")
-  }
+  console.log("chat id: " + chat_id)
+  Database.userLocations(chat_id).then(function (userLocations) {
+    if (userLocations[0] != null) getLocation(ctx, "Do you want to change your position?", "Change you location")
+    else getLocation(ctx, intro_message, "Set location")
+  })
 
 })
 
-
-/*bot.action(/.+/, (ctx) => {
+/*
+bot.action(/.+/, (ctx) => {
+  console.log("ahh")
   return ctx.answerCallbackQuery(`Oh, ${ctx.match[0]}! Great choise`)
 })*/
 
